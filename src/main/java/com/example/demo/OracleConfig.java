3b17fc92760a011255a1c5800b6088c28e0f6518
Huseyin BABAL <huseyin.babal@aurea.com>
2017-10-06 16:53:42 +0300

    Initial Project

---
+++ src/main/java/com/example/demo/OracleConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DbConfig {
+
+    private String url = "jdbc:oracle:thin:@oracle_db:1521/test";
+
+
+}
